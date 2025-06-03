20b9e7391d69faeb3871d0583f1131109caaaa6e
jhenni <jhenni262@gmail.com>
2025-06-03 09:42:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from supabase import create_client, Client
 from PIL import Image
 import io
 
-# Configuração do Supabase
-url = st.secrets["SUPABASE_URL"]
-key = st.secrets["SUPABASE_KEY"]
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
 supabase: Client = create_client(url, key)
 
 # Função para converter imagem para base64
